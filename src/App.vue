7b17e12770d80dc9a9f0d97c350c7a66a0cf770f
Maxwell Alvord <maxwellalvord@gmail.com>
2022-10-12 09:53:56 -0700

    complete functionality but breaking change causing bug

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,18 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..."/>
+        <input 
+        type="text"
+        class="search-bar" 
+        placeholder="Search..."
+        v-model="query"
+        @keypress="fetchWeather"
+        />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Northampton, UK</div>
+          <div class="location">{{ weather.name }},  {{ weather.sys.country }}</div>
           <div class="date">Monday 20 January 2022</div>
         </div>
 
@@ -21,16 +27,35 @@
 </template>
 
 <script>
+import e from 'express';
+
 
 
 export default {
   name: 'app',
   data () {
     return {
-      api_key: 'ca9b09be493e9b490939600e5793454f'
+      api_key: 'ca9b09be493e9b490939600e5793454f',
+      url_base: 'http://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    fetchWeather() {
+      if (e.key == 'Enter') {
+        fetch(`${this.url_base}weather?=${this.query}&appid=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults)
+      }
+    },
+    setResults(results) {
+      this.weather= results;
     }
   }
 }
+
 </script>
 
 <style>
@@ -88,4 +113,48 @@ main {
   border-radius: 16px 0px 16px 0px;
 }
 
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box{
+  text-align: center;
+  margin-top: 30px;
+}
+
+.weather-box .temp {
+  display: inline;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  margin-top: 30px;
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
 </style>
